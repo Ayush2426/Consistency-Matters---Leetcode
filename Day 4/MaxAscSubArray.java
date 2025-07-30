3c6256fddc49afa461c7601284f29aecfc021be7
Ayush Verma <av874559@gmail.com>
2025-07-30 22:56:07 +0530

    Git PR and merge resolved

--- Day 4/MaxAscSubArray.java
+++ Day 4/MaxAscSubArray.java
@@ -1,29 +1,3 @@
 public class MaxAscSubArray {
-<<<<<<< HEAD
     
-=======
-    public boolean TriMaxSeq(int[] nums){
-        int f = Integer.MAX_VALUE, s = Integer.MAX_VALUE, t = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < nums.length; i++) {
-            int k = nums[i];
-
-            if(k <= f){
-                f = k;
-            }else if(k <= s){
-                s = k;
-            }else{
-                t = k;
-                return true;
-            }
-            break;
-        }
-
-        return false;
-    }
-    public static void main(String[] args) {
-        MaxAscSubArray masa = new MaxAscSubArray();
-        masa.TriMaxSeq(new int[]{1, 3, 4, 2, 5, 8, 1});
-    }
->>>>>>> 684b3af41df3036d2e693ad6081f32be1e729cfd
 }
