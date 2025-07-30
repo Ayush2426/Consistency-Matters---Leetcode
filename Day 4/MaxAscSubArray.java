2e002103a20346e68073475ff169e6b459d1b3f7
Ayush Verma <av874559@gmail.com>
2025-07-30 23:17:41 +0530

    Everything resolved for Day 4

--- Day 4/MaxAscSubArray.java
+++ Day 4/MaxAscSubArray.java
@@ -5,7 +5,7 @@ public class MaxAscSubArray {
         int max = nums[1];
         int curMax = nums[1];
         
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
             if(nums[i - 1] < nums[i]){
                 curMax += nums[i];
             }else{
