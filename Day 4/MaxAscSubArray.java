583e082f140ecad569a115b2cc8a6b375e4b6724
Ayush Verma <av874559@gmail.com>
2025-07-30 23:02:15 +0530

    Day 4 4th code done

--- Day 4/MaxAscSubArray.java
+++ Day 4/MaxAscSubArray.java
@@ -1,3 +1,24 @@
+
+
 public class MaxAscSubArray {
-    
+    public int MaxAscSub(int[] nums){
+        int max = nums[1];
+        int curMax = nums[1];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i - 1] < nums[i]){
+                curMax += nums[i];
+            }else{
+                max = Math.max(max, curMax);
+                curMax = nums[i];
+            }
+            max = Math.max(max, curMax);
+        }
+
+        return max;
+    }
+    public static void main(String[] args) {
+        MaxAscSubArray masa = new MaxAscSubArray();
+        masa.MaxAscSub(new int[]{10, 20, 30, 40, 21, 2});
+    }
 }
