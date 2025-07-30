6ac666c4cb5b5ac089a768170d38924e0b726327
Ayush Verma <av874559@gmail.com>
2025-07-30 23:17:25 +0530

    Day 4 5th code done

--- Day 4/LuckyInteger.java
+++ Day 4/LuckyInteger.java
@@ -1,3 +1,28 @@
+
+import java.util.HashMap;
+
 public class LuckyInteger {
-    
+    public int findLucky(int[] nums){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; nums.length < 10; i++) {
+            int k = nums[i];
+
+            if(map.containsKey(k)){
+                map.put(k, map.get(k)+1);
+            }else{
+                map.put(k, 1);
+            }
+        }
+        int ans = -1;
+        for(int i : map.keySet()){
+            if(map.get(i) == i){
+                ans = Math.max(ans, i);
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        LuckyInteger li = new LuckyInteger();
+        li.findLucky(new int[]{1, 2, 2, 3, 4});
+    }
 }
