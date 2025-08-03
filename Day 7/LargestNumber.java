87caf2c7f1115e05f8b1c101b9ccaef3c245613b
Ayush Verma <av874559@gmail.com>
2025-08-03 21:44:15 +0530

    Day 7 2nd code done

---
+++ Day 7/LargestNumber.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+
+public class LargestNumber {
+    public String LargestStrCombination(int[] nums){
+        String arr[] = new String[nums.length];
+
+        for(int i = 0; i < nums.length; i++){
+            arr[i] = nums[i] + "";
+        }
+
+        Arrays.sort(arr, (s1, s2) -> (s1 + s2).compareTo(s2 + s1));
+
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        LargestNumber ln = new LargestNumber();
+        ln.LargestStrCombination(new int[]{1, 2, 33, 4, 5, 1, 3, 7, 99});
+    }
+}
