3e07c1b7aae548c8a9d7a0ed41b54fe49e981c39
Ayush Verma <av874559@gmail.com>
2025-08-03 21:44:01 +0530

    Day 2 4th code done

--- Day 2/MissingSum.java
+++ Day 2/MissingSum.java
@@ -2,7 +2,7 @@ public class MissingSum {
     public int findMissingSum(int[] nums){
         int n = nums.length;
         int currentSum = 0;
-        int actualSum = n * (n +1) / 2;
+        int actualSum = n * (n + 1) / 2;
         for(int num : nums){
             currentSum += num;
         }
@@ -10,7 +10,7 @@ public class MissingSum {
     }
     public static void main(String[] args) {
         MissingSum ms = new MissingSum();
-        int[] nums = {0, 1, 2, 3, 5}; // Example input
+        int[] nums = {0, 1, 2, 3, 5}; 
         int missingSum = ms.findMissingSum(nums);
         System.out.println("The missing sum is: " + missingSum);
     }
