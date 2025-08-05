11c8c1a596ac5965e26fee02b7a604f1d79536ea
Ayush Verma <av874559@gmail.com>
2025-08-05 23:19:36 +0530

    Day 8 5th code done

---
+++ Day 8/LongConsSeq.java
@@ -0,0 +1,34 @@
+
+import java.util.HashMap;
+
+public class LongConsSeq {
+    public int consSeqLong(int[]  nums){
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        int maxStreak = 0;    
+
+        for(int k : nums){
+            map.put(k, false);
+        }
+        for (int k : map.keySet()) {
+            if(map.containsKey(k - 1) == false){
+                map.put(k, true);
+            }
+        }
+
+        for(int k : map.keySet()){
+            int tempStreak = 1;
+            if(map.get(k) == true){
+                while(map.containsKey(k + tempStreak)){
+                    tempStreak++;
+                }
+            }
+
+            maxStreak = Math.max(tempStreak, maxStreak);
+        }
+        return  maxStreak;
+    }
+    public static void main(String[] args) {
+        LongConsSeq lcs = new LongConsSeq();
+        lcs.consSeqLong(new int[]{1, 2, 32, 33, 43, 3, 4, 6, 5, 7});
+    }
+}
