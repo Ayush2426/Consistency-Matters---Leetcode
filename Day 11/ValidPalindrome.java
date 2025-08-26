b9313f5aa09613d0dccecdf891edfe26b50b0918
Ayush Verma <av874559@gmail.com>
2025-08-26 23:31:03 +0530

    Day 11 2nd code done

---
+++ Day 11/ValidPalindrome.java
@@ -0,0 +1,37 @@
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c <= 'a' && c >= 'z') {
+                sb.append(c);
+            } else if (c >= 'A' && c <= 'Z') {
+                c = (char) (c - 'A' + 'a');
+                sb.append(c);
+            } else if (c >= '0' && c <= '9') {
+                sb.append(c);
+            }
+        }
+
+        int st = 0, end = sb.length() - 1;
+
+        while (st < end) {
+            int lc = sb.charAt(st);
+            int rc = sb.charAt(end);
+            if (lc != rc) {
+                return false;
+            }
+            st++;
+            end--;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        vp.isPalindrome("r ad a r");
+    }
+}
