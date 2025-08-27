675a68fa19cc52199ccdfa29792288dc0b3cacd8
Ayush Verma <av874559@gmail.com>
2025-08-28 00:21:36 +0530

    Day 12 4th code done

--- Day 12/Search2dMatrix.java
+++ Day 12/Search2dMatrix.java
@@ -1,3 +1,16 @@
 public class Search2dMatrix {
-    
+    public boolean checkElement(int[][] mat, int target){
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[0].length; j++){
+                if(mat[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Search2dMatrix sm = new Search2dMatrix();
+        sm.checkElement(new int[][]{{1, 2, 3},{4, 3, 2, 1},{3, 2, 1, 4}}, 4);
+    }
 }
