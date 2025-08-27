f48a6f8fae5abeb83ce7e7b4215b8c9a64f8a4be
Ayush Verma <av874559@gmail.com>
2025-08-28 00:09:24 +0530

    Day 12 3rd code done

--- Day 12/SpiralMatrixII.java
+++ Day 12/SpiralMatrixII.java
@@ -1,29 +1,29 @@
 public class SpiralMatrixII {
-    public int[][] getSpiralMatrix(int n){
+    public int[][] getSpiralMatrix(int n) {
         int[][] arr = new int[n][n];
 
         int count = 1;
         int minRow = 0, minCol = 0;
         int maxRow = n - 1, maxCol = n - 1;
 
-        while(count <= n*n){
+        while (count <= n * n) {
 
-            for(int i = minCol; i <= maxCol; i++){
+            for (int i = minCol; i <= maxCol; i++) {
                 arr[minRow][i] = count;
                 count++;
             }
 
-            for(int j = minRow + 1; j <= maxRow; j++ ){
-                arr[j][minCol] = count;
+            for (int j = minRow + 1; j <= maxRow; j++) {
+                arr[j][maxCol] = count;
                 count++;
             }
 
-            for(int i = maxCol - 1; i >= minCol; i++){
+            for (int i = maxCol - 1; i >= minCol; i--) {
                 arr[maxRow][i] = count;
                 count++;
             }
 
-            for(int j = maxRow - 1; j >= minRow + 1; j--){
+            for (int j = maxRow - 1; j >= minRow + 1; j--) {
                 arr[j][minCol] = count;
                 count++;
             }
@@ -35,6 +35,7 @@ public class SpiralMatrixII {
         }
         return arr;
     }
+
     public static void main(String[] args) {
         SpiralMatrixII smii = new SpiralMatrixII();
         smii.getSpiralMatrix(4);
