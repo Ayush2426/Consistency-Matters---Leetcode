5ec309ab3743151d4b085af32ce9307856d76b29
Ayush Verma <av874559@gmail.com>
2025-08-27 23:17:41 +0530

    day 12 3rd code done

--- Day 12/SpiralMatrixII.java
+++ Day 12/SpiralMatrixII.java
@@ -1,3 +1,42 @@
 public class SpiralMatrixII {
-    
+    public int[][] getSpiralMatrix(int n){
+        int[][] arr = new int[n][n];
+
+        int count = 1;
+        int minRow = 0, minCol = 0;
+        int maxRow = n - 1, maxCol = n - 1;
+
+        while(count <= n*n){
+
+            for(int i = minCol; i <= maxCol; i++){
+                arr[minRow][i] = count;
+                count++;
+            }
+
+            for(int j = minRow + 1; j <= maxRow; j++ ){
+                arr[j][minCol] = count;
+                count++;
+            }
+
+            for(int i = maxCol - 1; i >= minCol; i++){
+                arr[maxRow][i] = count;
+                count++;
+            }
+
+            for(int j = maxRow - 1; j >= minRow + 1; j--){
+                arr[j][minCol] = count;
+                count++;
+            }
+
+            minCol++;
+            maxCol--;
+            minRow++;
+            maxRow--;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        SpiralMatrixII smii = new SpiralMatrixII();
+        smii.getSpiralMatrix(4);
+    }
 }
