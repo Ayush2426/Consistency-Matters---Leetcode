f633db01841cf4e58b6ccac6356785dcecb87625
Ayush Verma <av874559@gmail.com>
2025-08-27 22:16:20 +0530

    Day 12 2nd code done

--- Day 12/TransposeMatrix.java
+++ Day 12/TransposeMatrix.java
@@ -1,3 +1,19 @@
 public class TransposeMatrix {
-    
+    public int[][] getTransposeMatrix(int[][] mat){
+
+        int cols = mat[0].length;
+        int rows = mat.length;
+        int[][] arr = new int[cols][rows];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[0].length; j++){
+                arr[j][i] = mat[i][j];
+            }
+        }
+        return mat;
+    }
+    public static void main(String[] args) {
+        TransposeMatrix tm = new TransposeMatrix();
+        tm.getTransposeMatrix(new int[][]{{1, 2, 3},{4, 5, 6},{7, 8, 9}});
+    }
 }
