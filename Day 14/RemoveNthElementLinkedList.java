b53f0275deb89e905ef01279112747ffb743a185
Ayush Verma <av874559@gmail.com>
2025-08-31 15:31:03 +0530

    Day 14 3rd code done

---
+++ Day 14/RemoveNthElementLinkedList.java
@@ -0,0 +1,35 @@
+// public class RemoveNthElementLinkedList {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+//         if (head.next == null) {
+//             head = null;
+//             return head;
+//         }
+
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while (n > 0) {
+//             fast = fast.next;
+//             n--;
+//         }
+
+//         if (fast == null) {
+//             return head.next;
+//         }
+
+//         while (fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next;
+//         }
+
+//         slow.next = slow.next.next;
+
+//         return head;
+
+//     }
+//     public static void main(String[] args) {
+//         RemoveNthElementLinkedList rnell = new RemoveNthElementLinkedList();
+//         rnell.removeNthFromEnd(null, 0);
+//     }
+// }
