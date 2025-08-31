ea5b2573bdbf2ff1634af2c380a5bc00af4f6d29
Ayush Verma <av874559@gmail.com>
2025-08-31 15:32:29 +0530

    Day 14 4th code done

---
+++ Day 14/DeleteNodeLinkedList.java
@@ -0,0 +1,10 @@
+// public class DeleteNodeLinkedList {
+//     public void deleteNode(ListNode node) {
+//         node.val = node.next.val;
+//         node.next = node.next.next;
+//    }
+//    public static void main(String[] args) {
+//     DeleteNodeLinkedList dnll = new DeleteNodeLinkedList;
+//     dnll.deleteNode(dnll);
+//    }
+// }
